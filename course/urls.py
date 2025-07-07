f5dafc450df386694e4257333d1952930a89c447
Stark1904 <tembotatenda006@gmail.com>
2025-07-07 17:33:27 +0200

    "Second Commit"

--- course/urls.py
+++ course/urls.py
@@ -4,10 +4,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', login_required(views.index), name='program.index'),
-    path('create/', login_required(views.create), name='program.create'),
-    path('store/', login_required(views.store), name="program.store"),
-    path('edit/<int:cid>/', login_required(views.edit), name="program.edit"),
-    path('update/<int:cid>/', login_required(views.update), name="program.update"),
-    path('delete/<int:cid>/', login_required(views.delete), name="program.delete"),
+    path('', login_required(views.index), name='course.index'),
+    path('create/', login_required(views.create), name='course.create'),
+    path('store/', login_required(views.store), name="course.store"),
+    path('edit/<int:cid>/', login_required(views.edit), name="course.edit"),
+    path('update/<int:cid>/', login_required(views.update), name="course.update"),
+    path('delete/<int:cid>/', login_required(views.delete), name="course.delete"),
 ]
--- student/urls.py
+++ student/urls.py
@@ -4,10 +4,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', login_required(views.index), name='participant.index'),
-    path('create/', login_required(views.create), name="participant.create"),
-    path('store/', login_required(views.store), name="participant.store"),
-    path('edit/<int:sid>/', login_required(views.edit), name="participant.edit"),
-    path('update/<int:sid>/', login_required(views.update), name="participant.update"),
-    path('delete/<int:sid>/', login_required(views.delete), name="participant.delete"),
+    path('', login_required(views.index), name='student.index'),
+    path('create/', login_required(views.create), name="student.create"),
+    path('store/', login_required(views.store), name="student.store"),
+    path('edit/<int:sid>/', login_required(views.edit), name="student.edit"),
+    path('update/<int:sid>/', login_required(views.update), name="student.update"),
+    path('delete/<int:sid>/', login_required(views.delete), name="student.delete"),
 ]
